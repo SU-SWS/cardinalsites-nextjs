b4581492c9560f1572f88d4adba69d73b310a8f5
Mike Decker <pookmish@gmail.com>
2024-03-06 21:43:57 -0800

    Add pagination list component

--- src/components/views/stanford-news/news-list-view.tsx
+++ src/components/views/stanford-news/news-list-view.tsx
@@ -1,6 +1,6 @@
 import StanfordNewsListItem from "@components/nodes/list-item/stanford-news/stanford-news-list-item";
-import LoadMoreList from "@components/elements/load-more-list";
 import {NodeStanfordNews} from "@lib/gql/__generated__/drupal.d";
+import PagedList from "@components/elements/paged-list";
 
 interface Props {
   headingLevel?: "h2" | "h3"
@@ -9,15 +9,14 @@ interface Props {
 
 const NewsListView = async ({items = [], headingLevel}: Props) => {
   return (
-    <LoadMoreList
-      buttonText={<>Load More<span className="sr-only">&nbsp;news</span></>}
+    <PagedList
       ulProps={{className: "list-unstyled mb-20"}}
       liProps={{className: "border-b border-black-20 last-of-type:border-0 pb-10 last:pb-0 pt-10 first:pt-0"}}
     >
       {items.map(item =>
         <StanfordNewsListItem key={item.id} node={item} headingLevel={headingLevel}/>
       )}
-    </LoadMoreList>
+    </PagedList>
   )
 }
 export default NewsListView;
