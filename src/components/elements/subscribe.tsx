a87b874b987f0b42894ca6018fb71cc0aa3e68da
Jen Breese-Kauth <jbreese@stanford.edu>
2024-03-28 16:16:42 +0000

    lint fixup

--- src/components/elements/subscribe.tsx
+++ src/components/elements/subscribe.tsx
@@ -4,7 +4,7 @@ const Subscribe = () => {
   return (
     <form>
     <input className="h-[3.5rem] w-[25rem] block type-0" type="email" id="" aria-label="signup email" name="signup email" placeholder="email address" />
-    <button href="/" className="button m-t-3 my-2 h-[35px]" type="submit" id="signup-submit">
+    <button className="button m-t-3 my-2 h-[35px]" type="submit" id="signup-submit">
       Sign-up
     </button>
   </form>
