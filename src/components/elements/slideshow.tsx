28461ec1f79aa0eb010c6b1a2f199a95df6848d4
pookmish <pookmish@gmail.com>
2024-04-05 07:42:54 -0700

    Adjust slideshow with styles and no infinite (#12)

--- src/components/elements/slideshow.tsx
+++ src/components/elements/slideshow.tsx
@@ -1,51 +1,58 @@
-'use client';
-import React, { HTMLAttributes, useRef } from 'react';
-import Slider, { CustomArrowProps, Settings } from 'react-slick';
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/16/solid';
-import { twMerge } from 'tailwind-merge';
+"use client";
 
-const NextArrow = ({ onClick }: CustomArrowProps) => {
+import {HTMLAttributes, JSX} from "react";
+import Slider, {CustomArrowProps, Settings} from "react-slick";
+import {ArrowLeftIcon, ArrowRightIcon} from "@heroicons/react/16/solid";
+import {twMerge} from "tailwind-merge";
+import {clsx} from "clsx";
+
+
+const NextArrow = ({className, onClick}: CustomArrowProps) => {
+  const slickDisabled = !!(className && className?.indexOf("slick-disabled") > 0);
   return (
     <button
-      className='absolute top-1/3 right-1 z-50'
+      className="absolute top-1/3 right-1 z-50"
       onClick={onClick}
-      aria-label='Next'
+      aria-label="Next"
+      disabled={slickDisabled}
     >
-      <ArrowRightIcon className='w-50 text-black-true bg-teal rounded-full p-10' />
+      <ArrowRightIcon className={twMerge(clsx("w-50 text-black-true bg-teal rounded-full p-10", {"text-black-50": slickDisabled}))}/>
     </button>
   );
 };
 
-const PrevArrow = ({ onClick }: CustomArrowProps) => {
+const PrevArrow = ({className, onClick}: CustomArrowProps) => {
+  const slickDisabled = !!(className && className?.indexOf("slick-disabled") > 0);
   return (
     <button
-      className='absolute top-1/3 left-1 z-50'
+      className="absolute top-1/3 left-1 z-50"
       onClick={onClick}
-      aria-label='Previous'
+      aria-label="Previous"
+      disabled={slickDisabled}
     >
-      <ArrowLeftIcon className='w-50 text-black-true bg-teal rounded-full p-10' />
+      <ArrowLeftIcon className={twMerge(clsx("w-50 text-black-true bg-teal rounded-full p-10", {"text-black-50": slickDisabled}))}/>
     </button>
   );
 };
 
 type SlideshowProps = HTMLAttributes<HTMLDivElement> & {
-  children: React.JSX.Element;
+  children: JSX.Element | JSX.Element[];
   slideshowProps?: Omit<Settings, "children">;
 }
 
-const Slideshow = ({ children, slideshowProps, ...props }: SlideshowProps) => {
-  const arrowRef = useRef<Slider>(null);
+const Slideshow = ({children, slideshowProps, ...props}: SlideshowProps) => {
   const settings: Settings = {
-    className: 'center',
-    centerMode: true,
+    autoplay: false,
+    centerMode: false,
+    className: "overflow-hidden pb-8 [&_.slick-slide]:float-left [&_.slick-slide]:px-5",
     dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+    infinite: false,
     initialSlide: 0,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow/>,
+    prevArrow: <PrevArrow/>,
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    speed: 500,
     responsive: [
       {
         breakpoint: 992,
@@ -59,8 +66,8 @@ const Slideshow = ({ children, slideshowProps, ...props }: SlideshowProps) => {
     ...slideshowProps,
   };
   return (
-    <div {...props} className={twMerge('relative w-full', props.className)}>
-      <Slider ref={arrowRef} {...settings}>
+    <div {...props} className={twMerge("relative w-full", props.className)}>
+      <Slider {...settings}>
         {children}
       </Slider>
     </div>
