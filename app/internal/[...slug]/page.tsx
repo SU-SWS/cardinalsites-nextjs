3d468375fa38c7cd5ebe879c8463d366627df094
Mike Decker <pookmish@stanford.edu>
2025-10-01 19:28:33 -0700

    static build paths

--- app/internal/[...slug]/page.tsx
+++ app/internal/[...slug]/page.tsx
@@ -24,7 +24,9 @@ const Page = async (props: PageProps) => {
 export const generateStaticParams = async (): Promise<Array<Slug>> => {
   const pagesToBuild = parseInt(process.env.BUILD_PAGES || "0")
   if (pagesToBuild === 0) return []
-  const paths = (await getAllNodes()).map(node => node.path).filter(path => !!path) as Array<string>
+  const paths = (await getAllNodes())
+    .map(node => node.path)
+    .filter(path => path?.startsWith("/internal/")) as Array<string>
   const nodePaths = paths.map(path => ({slug: path.split("/").filter(part => !!part)}))
   return pagesToBuild < 0 ? nodePaths : nodePaths.slice(0, pagesToBuild)
 }
