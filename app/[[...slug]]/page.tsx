5dfadf3358930f05486a0e3ee920b1349cbac969
Mike Decker <pookmish@stanford.edu>
2026-04-15 11:55:47 -0700

    Fixed generateStaticParams for routes

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -25,7 +25,8 @@ const Page = async (props: PageProps) => {
 
 export const generateStaticParams = async (): Promise<Array<Slug>> => {
   const pagesToBuild = parseInt(process.env.BUILD_PAGES || "0")
-  if (pagesToBuild === 0) return []
+  if (pagesToBuild === 0) return [{slug: [""]}]
+
   const paths = (await getAllNodes())
     .map(node => node.path)
     .filter(path => !path?.startsWith("/internal")) as Array<string>
--- app/av-media/[...slug]/page.tsx
+++ app/av-media/[...slug]/page.tsx
@@ -45,12 +45,12 @@ const Page = async ({params}: {params: Promise<Param>}) => {
 }
 
 export const generateStaticParams = async () => {
-  // Only build pages if we should build everything by using -1 for BUILD_PAGES.
-  if (parseInt(process.env.BUILD_PAGES ?? "") >= 0) return []
-
   let fetchMore = true
   let after: AudioVisualQueryVariables["after"] = undefined
-  const slugs: Array<Param> = [{slug: []}]
+  const slugs: Array<Param> = [{slug: [""]}]
+
+  // Only build pages if we should build everything by using -1 for BUILD_PAGES.
+  if (!process.env.BUILD_PAGES || Number(process.env.BUILD_PAGES) >= -1) return slugs
 
   while (fetchMore) {
     const query: AudioVisualQuery = await graphqlClient().request<AudioVisualQuery, AudioVisualQueryVariables>(
