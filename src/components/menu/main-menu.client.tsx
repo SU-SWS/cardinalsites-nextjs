e2d4392eeba32487b2bb7074b7aa0b8def3a37ba
Mike Decker <pookmish@stanford.edu>
2026-08-26 22:23:16 -0700

    Consolidate main menu markup into a single client component
    Moves all JSX/styling for the main menu into main-menu.client.tsx,
    leaving main-menu.tsx as a thin server component that only fetches
    menu data and header links.

--- src/components/menu/main-menu.client.tsx
+++ src/components/menu/main-menu.client.tsx
@@ -3,13 +3,20 @@
 import useOutsideClick from "@hooks/useOutsideClick"
 import {ChevronDownIcon} from "@heroicons/react/20/solid"
 import {useBoolean, useEventListener} from "usehooks-ts"
-import {HTMLAttributes, ReactNode, useCallback, useEffect, useRef} from "react"
+import {RefObject, useCallback, useEffect, useRef} from "react"
 import {usePathname} from "next/navigation"
 import cn from "@lib/utils/className"
-import Link, {LinkProps} from "@components/elements/link"
-import {RefObject} from "react"
+import Link from "@components/elements/link"
+import SiteSearchForm from "@components/search/site-search-form"
+import {MenuItem as MenuItemType, StanfordBasicSiteSetting} from "@lib/gql/__generated__/graphql"
 
-export const MainMenuClientWrapper = ({children, ...props}: HTMLAttributes<HTMLUListElement>) => {
+type Props = {
+  hideSearch?: boolean
+  menuItems: MenuItemType[]
+  headerLinks?: StanfordBasicSiteSetting["suSiteHeaderLinks"]
+}
+
+const MainMenuClient = ({hideSearch, menuItems, headerLinks}: Props) => {
   const buttonRef = useRef<HTMLButtonElement>(null)
   const menuRef = useRef<HTMLDivElement>(null)
 
@@ -32,7 +39,7 @@ export const MainMenuClientWrapper = ({children, ...props}: HTMLAttributes<HTMLU
   useEventListener("keydown", handleEscape, menuRef as RefObject<HTMLDivElement>)
 
   return (
-    <nav {...props} ref={menuRef}>
+    <nav aria-label="Main Navigation" className="lg:centered" ref={menuRef}>
       <button
         ref={buttonRef}
         className="group absolute right-10 top-5 flex flex-col items-center lg:hidden"
@@ -69,22 +76,39 @@ export const MainMenuClientWrapper = ({children, ...props}: HTMLAttributes<HTMLU
           block: menuOpen,
         })}
       >
-        {children}
+        {!hideSearch && <SiteSearchForm className="px-10 lg:hidden" />}
+        {headerLinks?.[0]?.url && (
+          <ul className="list-unstyled mx-auto flex w-fit flex-wrap gap-10 pl-16 pt-5 lg:hidden">
+            {headerLinks.map((link, i) => (
+              <li key={`utility-link-${i}`}>
+                <Link className="text-white no-underline hocus:text-white hocus:underline" href={link.url as string}>
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+        <ul className="list-unstyled m-0 flex-wrap p-0 lg:flex lg:justify-end">
+          {menuItems.map(item => (
+            <MenuItem key={item.id} {...item} level={0} />
+          ))}
+        </ul>
       </div>
     </nav>
   )
 }
 
-type ItemProps = HTMLAttributes<HTMLLIElement> & {
+type MenuItemProps = MenuItemType & {
   level: number
-  link: ReactNode
 }
 
-export const MainMenuItemClientWrapper = ({id, level, link, children, ...props}: ItemProps) => {
+const MenuItem = ({id, url, title, children, level}: MenuItemProps) => {
   const menuItemRef = useRef<HTMLLIElement>(null)
   const buttonRef = useRef<HTMLButtonElement>(null)
   const {value: submenuOpen, setFalse: closeSubmenu, toggle: toggleSubmenu} = useBoolean(false)
   const browserUrl = usePathname()
+  const href = url || "#"
+  const isCurrent = href === browserUrl
 
   useOutsideClick(menuItemRef, closeSubmenu)
 
@@ -105,10 +129,37 @@ export const MainMenuItemClientWrapper = ({id, level, link, children, ...props}:
   useEventListener("keydown", handleEscape, menuItemRef as unknown as RefObject<HTMLDivElement>)
 
   return (
-    <li {...props} ref={menuItemRef}>
-      {link}
+    <li
+      ref={menuItemRef}
+      className={cn(
+        "relative m-0 grid grid-cols-10 items-center justify-between border-b border-cool-grey py-2 first:border-t last:border-0 lg:relative lg:border-black-20 lg:py-0 lg:pr-5",
+        {"first:border-t-0 lg:flex lg:border-b-0 last:lg:pr-0": level === 0, "lg:first:border-t-0": level === 1}
+      )}
+    >
+      <Link
+        prefetch={level === 0}
+        href={href}
+        data-intrail={!isCurrent && browserUrl.includes(href) && href !== "/"}
+        aria-current={isCurrent ? "page" : undefined}
+        className={cn(
+          "col-start-1 col-end-9 border-l-[6px] border-transparent py-5 text-white no-underline transition-all hocus:text-white hocus-visible:border-white hocus-visible:underline lg:text-digital-red lg:hocus:text-black",
+          {
+            "ml-5 pl-10 aria-current-page:border-digital-red data-intrail:border-transparent lg:ml-0 lg:border-b-[6px] lg:border-l-0 lg:pb-2 lg:pl-0 lg:aria-current-page:border-black lg:data-intrail:border-foggy-dark":
+              level === 0,
+            "pl-20 aria-current-page:border-digital-red lg:pl-5 lg:hocus-visible:border-black-true": level === 1,
+            "pl-28 aria-current-page:border-digital-red lg:pl-10 lg:hocus-visible:border-black-true": level === 2,
+            "pl-48 aria-current-page:border-digital-red lg:pl-20 lg:hocus-visible:border-black-true": level === 3,
+            "ml-5 aria-current-page:border-digital-red lg:ml-0 lg:hocus-visible:border-black-true": level !== 0,
+          }
+        )}
+      >
+        {title}
+      </Link>
+      {level === 0 && !!children.length && (
+        <span className="mb-[6px] ml-5 hidden h-[25px] w-[1px] bg-archway-light lg:block" />
+      )}
 
-      {children && (
+      {!!children.length && (
         <>
           <button
             aria-labelledby={id}
@@ -126,25 +177,22 @@ export const MainMenuItemClientWrapper = ({id, level, link, children, ...props}:
             />
           </button>
 
-          {submenuOpen && children}
+          {submenuOpen && (
+            <ul
+              className={cn("list-unstyled col-span-10 w-full min-w-[300px] px-0 lg:bg-white", {
+                "lg:absolute lg:right-0 lg:top-full lg:shadow-2xl": level === 0,
+                "lg:top-0": level !== 0,
+              })}
+            >
+              {children.map(item => (
+                <MenuItem key={item.id} {...item} level={level + 1} />
+              ))}
+            </ul>
+          )}
         </>
       )}
     </li>
   )
 }
 
-export const MainMenuItemClientLink = ({href, children, ...props}: LinkProps) => {
-  const currentPath = usePathname()
-  const isCurrent = href === currentPath
-
-  return (
-    <Link
-      {...props}
-      href={href}
-      data-intrail={!isCurrent && currentPath.includes(href) && href !== "/"}
-      aria-current={isCurrent ? "page" : undefined}
-    >
-      {children}
-    </Link>
-  )
-}
+export default MainMenuClient
--- src/components/menu/main-menu.tsx
+++ src/components/menu/main-menu.tsx
@@ -1,13 +1,6 @@
 import {getConfigPageField, getMenu} from "@lib/gql/gql-queries"
-import {MenuAvailable, MenuItem as MenuItemType, StanfordBasicSiteSetting} from "@lib/gql/__generated__/graphql"
-import cn from "@lib/utils/className"
-import SiteSearchForm from "@components/search/site-search-form"
-import {
-  MainMenuClientWrapper,
-  MainMenuItemClientLink,
-  MainMenuItemClientWrapper,
-} from "@components/menu/main-menu.client"
-import Link from "@components/elements/link"
+import {MenuAvailable, StanfordBasicSiteSetting} from "@lib/gql/__generated__/graphql"
+import MainMenuClient from "@components/menu/main-menu.client"
 
 type Props = {
   hideSearch?: boolean
@@ -21,82 +14,7 @@ const MainMenu = async ({hideSearch}: Props) => {
   )
   if (!menuItems.length && !headerLinks?.length && hideSearch) return null
 
-  return (
-    <MainMenuClientWrapper aria-label="Main Navigation" className="lg:centered">
-      {!hideSearch && <SiteSearchForm className="px-10 lg:hidden" />}
-      {headerLinks?.[0].url && (
-        <ul className="list-unstyled mx-auto flex w-fit flex-wrap gap-10 pl-16 pt-5 lg:hidden">
-          {headerLinks.map((link, i) => (
-            <li key={`utility-link-${i}`}>
-              <Link className="text-white no-underline hocus:text-white hocus:underline" href={link.url as string}>
-                {link.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-      <ul className="list-unstyled m-0 flex-wrap p-0 lg:flex lg:justify-end">
-        {menuItems.map(item => (
-          <MenuItem key={item.id} {...item} level={0} />
-        ))}
-      </ul>
-    </MainMenuClientWrapper>
-  )
-}
-
-type MenuItemProps = MenuItemType & {
-  level: number
-}
-
-const MenuItem = ({id, url, title, children, level}: MenuItemProps) => {
-  return (
-    <MainMenuItemClientWrapper
-      id={id}
-      level={level}
-      className={cn(
-        "relative m-0 grid grid-cols-10 items-center justify-between border-b border-cool-grey py-2 first:border-t last:border-0 lg:relative lg:border-black-20 lg:py-0 lg:pr-5",
-        {"first:border-t-0 lg:flex lg:border-b-0 last:lg:pr-0": level === 0, "lg:first:border-t-0": level === 1}
-      )}
-      link={
-        <>
-          <MainMenuItemClientLink
-            prefetch={level === 0}
-            id={id}
-            href={url || "#"}
-            className={cn(
-              "col-start-1 col-end-9 border-l-[6px] border-transparent py-5 text-white no-underline transition-all hocus:text-white hocus-visible:border-white hocus-visible:underline lg:text-digital-red lg:hocus:text-black",
-              {
-                "ml-5 pl-10 aria-current-page:border-digital-red data-intrail:border-transparent lg:ml-0 lg:border-b-[6px] lg:border-l-0 lg:pb-2 lg:pl-0 lg:aria-current-page:border-black lg:data-intrail:border-foggy-dark":
-                  level === 0,
-                "pl-20 aria-current-page:border-digital-red lg:pl-5 lg:hocus-visible:border-black-true": level === 1,
-                "pl-28 aria-current-page:border-digital-red lg:pl-10 lg:hocus-visible:border-black-true": level === 2,
-                "pl-48 aria-current-page:border-digital-red lg:pl-20 lg:hocus-visible:border-black-true": level === 3,
-                "ml-5 aria-current-page:border-digital-red lg:ml-0 lg:hocus-visible:border-black-true": level !== 0,
-              }
-            )}
-          >
-            {title}
-          </MainMenuItemClientLink>
-          {level === 0 && !!children.length && (
-            <span className="mb-[6px] ml-5 hidden h-[25px] w-[1px] bg-archway-light lg:block" />
-          )}
-        </>
-      }
-    >
-      {!!children.length && (
-        <ul
-          className={cn("list-unstyled col-span-10 w-full min-w-[300px] px-0 lg:bg-white", {
-            "lg:absolute lg:right-0 lg:top-full lg:shadow-2xl": level === 0,
-            "lg:top-0": level !== 0,
-          })}
-        >
-          {children.map(item => (
-            <MenuItem key={item.id} {...item} level={level + 1} />
-          ))}
-        </ul>
-      )}
-    </MainMenuItemClientWrapper>
-  )
+  return <MainMenuClient hideSearch={hideSearch} menuItems={menuItems} headerLinks={headerLinks} />
 }
 
 export default MainMenu
